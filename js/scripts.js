5b64b4eb5fac4778e030030b1d809352cb4e6a2d
Colin Worf <colin.worf@gmail.com>
2017-10-27 16:47:08 -0700

    enhance scroll effect with colors and blinking

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@ function count(countTo, name){
     } else if (/0/.test(number)) {
       return "Beep!";
     } else {
-      return number;
+      return "Pod Bay door " + number + " is opening...";
     }
   });
 
@@ -25,21 +25,48 @@ $(function(){
     var userInput = parseInt($('#user-input').val());
     var userName = $('#user-name').val();
     var result = count(userInput, userName);
-console.log(result);
-    for (i = 0; i < result.length; i++){
+
+    if (userName) {
+      for (i = 0; i < result.length; i++){
       (function(i){
+        var glow = "";
+        var color = "";
+            if (/!/.test(result[i])){
+              glow = "inset 0 0 111px rgba(245, 66, 0, 0.84)";
+              color = "#f54200";
+            } else if (/sorry/.test(result[i])) {
+              glow = "inset 0 0 111px red";
+              color = "red";
+            } else {
+              glow = "inset 0 0 111px rgba(0, 245, 56, 0.78)";
+              color = "green";
+            }
         setTimeout(function(){
-          $('.output-box').append("<div class='start'>" + result[i] + "</div>");
+          $('.output-box').append("<div class='start' style='color:" + color + ";'>" + result[i] + "</div>");
           $('.start').addClass("bottom").delay(10).queue(function(next){
             $(this).addClass('up');
             next();
           });
-          $('.output-box').addClass("flash").delay(10).queue(function(next){
-            $(this).removeClass('flash');
+          $('.output-box').addClass("flash").css('box-shadow', glow).delay(10).queue(function(next){
+            $(this).removeClass('flash').css("box-shadow", "");
             next();
           });
         }, 2000 * i);
       }(i));
+    }
+    } else {
+      for (i = 0; i < 15; i++){
+      (function(i){
+        setTimeout(function(){
+          $('.output-box').append("<div class='start' style='color:red; font-size:10px'>Not authorized!!</div>");
+          $('.start').addClass("bottom").delay(10).queue(function(next){
+            $(this).addClass('up');
+            next();
+          });
+          $('.output-box').addClass("flash").css('box-shadow', 'inset 0 0 111px red')
+        }, 200 * i);
+      }(i));
+    }
     }
   });
 });
